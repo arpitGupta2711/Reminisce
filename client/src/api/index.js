9a5a5a58046c0470f8937a736c73b9a1e700119f
Arpit <agvinayak27@gmail.com>
2023-04-09 21:44:50 +0530

    changed base url for client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API = axios.create({baseURL:'http://localhost:5000'})
+const API = axios.create({baseURL:'https://reminisce-api.vercel.app'})
 // const url = "http://localhost:5000/posts";
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
